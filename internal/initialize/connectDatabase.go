1f866635d3b673f02e216acf4a2df35b5d7b1080
kanzoonrekza <kanzoonrkz@gmail.com>
2024-07-03 16:55:16 +0700

    feat: create new trackr api and get all trackr api

--- internal/initialize/connectDatabase.go
+++ internal/initialize/connectDatabase.go
@@ -14,6 +14,8 @@ func ConnectDatabase() {
 	dsn := os.Getenv("DATABASE_URL")
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB.Exec("DEALLOCATE ALL")
+
 	if err != nil {
 		log.Fatal(err)
 	}
