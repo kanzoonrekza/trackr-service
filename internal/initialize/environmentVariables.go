74b053469a81be74228bcec8b1f2aaa2e17e4c6f
kanzoonrekza <kanzoonrkz@gmail.com>
2024-07-04 15:30:07 +0700

    fix: possible fix to railway deployment

--- internal/initialize/environmentVariables.go
+++ internal/initialize/environmentVariables.go
@@ -2,14 +2,22 @@ package initialize
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func EnvironmentVariables() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if os.Getenv("RAILWAY_ENVIRONMENT") == "" {
+		// We're not on Railway, so try to load from .env file
+		err := godotenv.Load()
+		log.Println("Using environment variables from .env file")
+		if err != nil {
+			log.Println("Warning: Error loading .env file. Using existing environment variables.")
+		} else {
+			log.Println("Loaded .env file")
+		}
+	} else {
+		log.Println("Running on Railway. Using provided environment variables.")
 	}
-	log.Println("Loaded .env file")
 }
