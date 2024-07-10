4fc35ae91752966e80d6c84227d7075eab5ec2ec
kanzoonrekza <kanzoonrkz@gmail.com>
2024-07-10 23:46:21 +0700

    hello route

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"trackr-service/internal/initialize"
@@ -20,6 +21,15 @@ func main() {
 		Handler: mux,
 	}
 	log.Println("Server listening on port 8080")
+
+	mux.HandleFunc("GET /api/hello", func(w http.ResponseWriter, r *http.Request) {
+		response := map[string]interface{}{
+			"message": "Hello from Trackr-Service!",
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	})
 
 	mux.HandleFunc("POST /api/user/login", services.UserLogin)
 	mux.HandleFunc("POST /api/user/register", services.UserRegister)
