47ab7475fa484fb9325683fb71c1f395ec0b82e9
kanzoonrekza <kanzoonrkz@gmail.com>
2024-07-11 07:06:17 +0700

    add cors middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -18,11 +18,11 @@ func main() {
 	mux := http.NewServeMux()
 	server := http.Server{
 		Addr:    ":8080",
-		Handler: mux,
+		Handler: utils.CorsMiddleware(mux),
 	}
 	log.Println("Server listening on port 8080")
 
-	mux.HandleFunc("GET /api/hello", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
 		response := map[string]interface{}{
 			"message": "Hello from Trackr-Service!",
 		}
