3306ed03a61461db336b83f85f9769390c7b973f
kanzoonrekza <kanzoonrkz@gmail.com>
2024-08-12 12:19:38 +0700

    ad status to trackr model

--- internal/models/trackrModel.go
+++ internal/models/trackrModel.go
@@ -7,7 +7,8 @@ type Trackr struct {
 	Title          string `gorm:"not null"`
 	TotalEpisode   uint16 `gorm:"not null"`
 	CurrentEpisode uint16 `gorm:"not null; default:1"`
-	UserID         uint   `gorm:"not null"`
+	Status         string `gorm:"not null; default:'Plan to Watch'"`
 	Completed      bool   `gorm:"not null; default:false"`
 	Rate           int8   `gorm:"not null; check:Rate >= 0 AND Rate <= 10"`
+	UserID         uint   `gorm:"not null"`
 }
